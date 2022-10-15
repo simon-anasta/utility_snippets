efcb0e8c7cbfa1c6dfecea640b2c0b9b92d9287d
Simon A <31526262+simon-anasta@users.noreply.github.com>
2022-10-15 15:24:58 +1300

    simple install packages

---
+++ R scripts/install_prefered_packages.R
@@ -0,0 +1,25 @@
+# Install all preferred packages
+#
+
+list_of_packages = c(
+  "dplyr",
+  "shiny",
+  "tidyr",
+  "ggplot2",
+  "dbplyr",
+  "DBI",
+  "explore",
+  "rlang",
+  "purrr",
+  "testthat",
+  "rmarkdown"
+)
+
+
+for(pkg in list_of_packages){
+  if(pkg %in% installed.packages()){
+    next
+  }
+  
+  install.packages(pkg)
+}
