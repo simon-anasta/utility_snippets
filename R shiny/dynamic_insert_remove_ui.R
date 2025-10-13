16f02e7117c1bb0c5160eb1ac3c57ce6a53e554a
Simon A <31526262+simon-anasta@users.noreply.github.com>
2025-10-13 14:27:23 +1300

    Add files via upload

---
+++ R shiny/dynamic_insert_remove_ui.R
@@ -0,0 +1,103 @@
+################################################################################
+# Designing dynamic tiles for terms
+# 
+# Each added tile is a button. Clicking a tile/button puts its contents into the
+# text box so it can be removed.
+# 
+# Original design used renderUI and uiOutput for dynamic / responsive rendering
+# of the tile-buttons. See 'dynamic_creation_detection_removal.R'.
+# However, this makes for a clumsy button listener, because it is trigger on
+# button creation & removal, not just on button click.
+# 
+# Inserting and removing UI components is a more elegant solution.
+# > It does require use of IDs for CSS/HTML identification of components, so is
+#     not pure R. But this straightforward: mostly just `paste0('#', id)`.
+# > It makes observers much simpler: when a new button-tile is created, an
+#     observer for the button-tile is created. The observer is created exactly
+#     once.
+# > There is no way to remove an observed. Once a button-tile is removed the
+#     observer sits idle as it can no longer be triggered. Unless a user was
+#     creating 100's of such tiles, it is unlikely that these idle observers
+#     will have any impact on performance.
+# 
+
+################################################################################
+
+library(shiny)
+
+## ui --------------------------------------------------------------------- ----
+
+ui <- fluidPage(
+  strong("Ignored terms"),
+  # div just taller than default button size
+  # needs id for insertUI
+  tags$div(id = "button_tiles", style = "height: 38px;"),
+  textInput("current_text", "Term to ignore"),
+  actionButton("action_add", "Add"),
+  actionButton("action_remove", "Remove"),
+  actionButton("action_remove_all", "Remove all"),
+  p("Internal view"),
+  textOutput("term_active")
+)
+
+## server ----------------------------------------------------------------- ----
+
+
+server <- function(input, output, session) {
+  
+  rv = reactiveValues(
+    active_terms = list(),
+    num_term_buttons = 0
+  )
+  
+  output$term_active <- renderText({
+    paste(rv$active_terms, collapse = "\n")
+  })
+  
+  observeEvent(input$action_add, {
+    req(nchar(input$current_text) > 0)
+    req(!input$current_text %in% rv$active_terms)
+
+    rv$num_term_buttons = rv$num_term_buttons + 1
+    id = paste0("term_button_", rv$num_term_buttons)
+    
+    insertUI(
+      selector = "#button_tiles",
+      where = "beforeEnd",
+      ui = actionButton(id, input$current_text)
+    )
+    
+    local({
+      btn_id = id
+      observeEvent(input[[btn_id]], {
+        updateTextInput(session, "current_text", value = rv$active_terms[[btn_id]])
+      })
+    })
+
+    rv$active_terms[[id]] = input$current_text
+    updateTextInput(session, "current_text", value = "")
+  })
+
+  observeEvent(input$action_remove, {
+    req(nchar(input$current_text) > 0)
+    req(input$current_text %in% rv$active_terms)
+    
+    id = names(rv$active_terms)[rv$active_terms == input$current_text]
+    removeUI(selector = paste0("#", id))
+    
+    rv$active_terms[[id]] = NULL
+    updateTextInput(session, "current_text", value = "")
+  })
+
+  observeEvent(input$action_remove_all, {
+    req(length(rv$active_terms) > 0)
+    
+    for(id in names(rv$active_terms)){
+      removeUI(selector = paste0("#", id))
+      rv$active_terms[[id]] = NULL
+    }
+  })
+
+}
+
+shinyApp(ui, server)
